e388a3db033f5d4d47700e11436d0ff887144066
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2022-08-29 17:28:51 +0600

    done

--- index.js
+++ index.js
@@ -22,9 +22,17 @@ function initMap(){
           lng: 90.4009,
           image: 'images/sajib.jpg',
           activity_log: [
-             {message:'Assign Abdus sattar to pickup - DM45454', time: ' 1 month ago'},
-             {message:'Pickup Done by Abdus sattar - DM45454', time: ' 1 month ago'},
-             {message:'delivered by Abdus sattar to pickup DM45454', time: ' 1 month ago'}
+             {message:'Assign সজিব আলি to pickup - DM45454', time: ' 1 month ago'},
+             {message:'Pickup Done by সজিব আলি - DM45454', time: ' 1 month ago'},
+             {message:'delivered by সজিব আলি to pickup DM45454', time: ' 1 month ago'}
+             ]
+        },
+        { name: 'Kamruzzaman',
+          lat: 23.7821,
+          lng: 90.4161,
+          image: 'images/avatar.png',
+          activity_log: [
+             
              ]
         },
     ];
@@ -33,25 +41,16 @@ for(var i=0; i< riders.length; i++ ){
 	 var rider_activity_info = '<h2>'+riders[i].name+'</h2>';
 
      rider_activity_info += '<ul>';
-     rider_activity_info +='<li><strong>DM45454</strong> Assign to pickup <span>24 hours ago</span></li>';
-     rider_activity_info +='<li><strong>DM45454</strong> Pickup done <span>12 hours ago</span></li>';
-     rider_activity_info +='<li><strong>DM45454</strong> delivered <span>20 minutes ago</span></li>';
+     for(var j=0; j<riders[i].activity_log.length; j++){
+         rider_activity_info +='<li>'+riders[i].activity_log[j].message+'<span>'+riders[i].activity_log[j].time+'</span></li>';
+     }
      rider_activity_info +='</ul>';
+     if(riders[i].activity_log.length == 0){
+           rider_activity_info ='<p>No activity found yet now.</p>';
+     }
      addMarker({location: {lat:riders[i].lat, lng:riders[i].lng}, icon:riders[i].image, activity_info: rider_activity_info});
 }
     
-    var rider_activity_info = '<h2> Abdus Sattar Bhuiyan</h2>';
-
-     rider_activity_info += '<ul>';
-     rider_activity_info +='<li><strong>DM45454</strong> Assign to pickup <span>24 hours ago</span></li>';
-     rider_activity_info +='<li><strong>DM45454</strong> Pickup done <span>12 hours ago</span></li>';
-     rider_activity_info +='<li><strong>DM45454</strong> delivered <span>20 minutes ago</span></li>';
-     rider_activity_info +='</ul>';
-
-	
-    
-    //addMarker({location: {lat:23.7812, lng:90.4039}, icon:'images/sattar.png', activity_info: rider_activity_info});
-
 	function addMarker(property){
 		const marker = new google.maps.Marker({
 		position:property.location,
